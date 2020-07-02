d19512589a40d983c6d36f9558c5c187531a1bd4
Panji <panjisap0805@gmail.com>
2020-07-03 01:41:39 +0700

    template

---
+++ seeders/20200702180926-seed-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const helper = require("../helpers/helper");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      username: 'admin',
+      password: helper.encrypt('admin'),
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date() 
+    }])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('Users', null, {})
+  }
+};
